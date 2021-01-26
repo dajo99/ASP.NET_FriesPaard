7b1a54f51b5a25288005435e647ae8a83391b863
Dajo Vandoninck <dajo.vandoninck99@gmail.com>
2021-01-26 03:28:57 +0100

    update + gitignore

--- BSFP/BSFP/Models/Agenda.cs
+++ BSFP/BSFP/Models/Agenda.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,14 @@ namespace BSFP.Models
         public int AgendaID { get; set; }
         public string Titel { get; set; }
         public string Omschrijving { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Datum { get; set; }
+
+        [DataType(DataType.Time)]
         public DateTime Starttijd { get; set; }
+        
+        [DataType(DataType.Time)]
         public DateTime Eindtijd { get; set; }
         public string Locatie { get; set; }
     }
